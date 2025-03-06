4f8bb9d6468abe99255a94c551b3f7029939b75e
Samuel P <samuel_png10@hotmail.com>
2025-03-06 21:38:41 +0800

    Revert "add timestamp logs"
    This reverts commit 4ed0f358acc0379eead98968d9b3e5839fe41547.

--- index.ts
+++ index.ts
@@ -1,13 +1,8 @@
-import * as dotenv from "dotenv";
-import {
-  ChannelType,
-  Client,
-  GatewayIntentBits,
-  TextChannel,
-} from "discord.js";
-import express from "express";
-import fetch from "node-fetch";
-import cron from "node-cron";
+import * as dotenv from 'dotenv';
+import { ChannelType, Client, GatewayIntentBits, TextChannel } from 'discord.js';
+import express from 'express';
+import fetch from 'node-fetch';
+import cron from 'node-cron';
 
 dotenv.config();
 const { DISCORD_TOKEN } = process.env;
@@ -15,72 +10,42 @@ const { DISCORD_TOKEN } = process.env;
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => {
-  const timestamp = new Date().toISOString();
-  console.log(`[${timestamp}] Ping!`);
+app.get('/', (req, res) => res.send('Hello World!'));
 
-  res.send("Hello World!");
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+const bot = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
+bot.once('ready', () => {
+    console.log('Ready!');
 });
 
-app.listen(port, () => {
-  const timestamp = new Date().toISOString();
-  console.log(
-    `[${timestamp}] Example app listening at http://localhost:${port}`
-  );
-});
-
-const bot = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
-});
-
-bot.once("ready", () => {
-  const timestamp = new Date().toISOString();
-  console.log(`[${timestamp}] Ready!`);
-});
-
-bot.on("voiceStateUpdate", async (oldState, newState) => {
-  if (oldState.channelId === null) {
-    let notifChannel = oldState.guild.channels.cache.find(
-      (channel) => channel.name === "vc-notif"
-    );
-    if (notifChannel === undefined) {
-      let voiceCategory = oldState.guild.channels.cache.find(
-        (channel) =>
-          channel.type == ChannelType.GuildCategory &&
-          channel.name.toUpperCase() == "Voice Channels".toUpperCase()
-      );
-      notifChannel = await oldState.guild.channels.create({
-        name: "vc-notif",
-        type: ChannelType.GuildText,
-        ...(voiceCategory !== undefined && { parent: voiceCategory.id }),
-      });
+bot.on('voiceStateUpdate', async (oldState, newState) => {
+    if (oldState.channelId === null) {
+        let notifChannel = oldState.guild.channels.cache.find(channel => channel.name === "vc-notif");
+        if (notifChannel === undefined) {
+            let voiceCategory = oldState.guild.channels.cache.find(channel => channel.type == ChannelType.GuildCategory && channel.name.toUpperCase() == "Voice Channels".toUpperCase());
+            notifChannel = await oldState.guild.channels.create({ name: 'vc-notif', type: ChannelType.GuildText, ...(voiceCategory !== undefined && { parent: voiceCategory.id }) });
+        }
+        let channelId = notifChannel.id;
+        (bot.channels.cache.get(channelId) as TextChannel).send(`@everyone ${newState.member?.displayName} has joined`);
     }
-    let channelId = notifChannel.id;
-    (bot.channels.cache.get(channelId) as TextChannel).send(
-      `@everyone ${newState.member?.displayName} has joined`
-    );
-
-    console.log(`${newState.member?.displayName} has joined`);
-  }
 });
 
 bot.login(DISCORD_TOKEN);
 
 // Function to ping the URL and log the response time
 async function pingURL() {
-  const url = "https://massive-nerissa-sammn97.koyeb.app/";
-  const start = Date.now();
-  try {
-    const response = await fetch(url);
-    const end = Date.now();
-    const responseTime = end - start;
-    console.log(
-      `[${new Date().toISOString()}] Ping to ${url} - Response time: ${responseTime}ms`
-    );
-  } catch (error) {
-    console.error(`[${new Date().toISOString()}] Error pinging ${url}:`, error);
-  }
+    const url = 'https://massive-nerissa-sammn97.koyeb.app/';
+    const start = Date.now();
+    try {
+        const response = await fetch(url);
+        const end = Date.now();
+        const responseTime = end - start;
+        console.log(`[${new Date().toISOString()}] Ping to ${url} - Response time: ${responseTime}ms`);
+    } catch (error) {
+        console.error(`[${new Date().toISOString()}] Error pinging ${url}:`, error);
+    }
 }
 
 // Schedule the ping task to run every 5 minutes
-cron.schedule("*/5 * * * *", pingURL);
+cron.schedule('*/5 * * * *', pingURL);
